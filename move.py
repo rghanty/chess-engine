9a4b88833e5d2a729b0388eb113e056b1027b930
Rishabh Ghanty <99227180+rghanty@users.noreply.github.com>
2024-06-25 13:35:18 +0530

    Added move class

---
+++ move.py
@@ -0,0 +1,21 @@
+class Move:
+    rankToRow = {str(i): 8-i for i in range(1,9)}           #{"1":7,"2":6 etc..}
+    rowToRank = {v:k for k,v in rankToRow.items()}
+
+    fileToCol = {chr(i+65):i for i in range(8)}             #{"a":0, "b":1 etc..}
+    colToFile = {v:k for k,v in fileToCol.items()}
+
+    def __init__(self, start, end, board):
+        self.startRow, self.startCol = start
+        self.endRow, self.endCol = end
+
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+    
+    def notation(self):
+        #can make this real chess notation
+        return self.getRankFile(self.startRow,self.startCol) + self.getRankFile(self.endRow, self.endCol)       #if move is pawn from a2 to a4, returns a2a4
+
+
+    def getRankFile(self,r,c):
+        return self.colToFile[c] + self.rowToRank[r]
