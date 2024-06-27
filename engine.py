a1f3ad9b4ece5a81448cfce16546be9b5e6eb483
Rishabh Ghanty <99227180+rghanty@users.noreply.github.com>
2024-06-27 16:03:36 +0530

    Added move generating logic for every piece

--- engine.py
+++ engine.py
@@ -37,10 +37,90 @@ class GameState:
                     piece = self.board[row][col]
                     colour,type = piece[0],piece[1]
                     if self.whiteTurn and colour =="w" or (not self.whiteTurn and colour=="b"):
-                        if type == "P":
-                            self.generatePawnMoves(row,col,allMoves)
-                        elif type == "R":
-                            self.generateRookMoves(row,col,allMoves)
+                        self.pieceFuncs[type](row,col,allMoves)
+        return allMoves
+
+    def generatePawnMoves(self,row,col,moves):
+        
+        if self.whiteTurn and row-1>=0:
+            if self.board[row-1][col] == "--":
+                moves.append(Move((row,col),(row-1,col),self.board))
+            if row == 6 and self.board[row-2][col] == "--":
+                moves.append(Move((row,col),(row-2,col),self.board))
+            
+            if col-1>=0 and self.board[row-1][col-1][0] == "b":
+                moves.append(Move((row,col),(row-1,col-1),self.board))
+            if col+1 <8 and self.board[row-1][col+1][0] == "b":
+                 moves.append(Move((row,col),(row-1,col+1),self.board))
+        
+        if not self.whiteTurn and row + 1 >= 0:
+            if self.board[row+1][col] == "--":
+                moves.append(Move((row,col),(row+1,col),self.board))
+            if row == 1 and self.board[row+2][col] == "--":
+                moves.append(Move((row,col),(row+2,col),self.board))
+            
+            if col-1>=0 and self.board[row+1][col-1][0] == "w":
+                moves.append(Move((row,col),(row+1,col-1),self.board))
+            if col+1 <8 and self.board[row+1][col+1][0] == "w":
+                moves.append(Move((row,col),(row+1,col+1),self.board))
+        
+
+    def generateRookMoves(self,row,col,moves):
+        
+        dirs = ((-1,0),(0,-1),(0,1),(1,0))
+        self.generateMovesWithDirs(row,col,dirs,moves)
+    
+    def generateBishopMoves(self,row,col,moves):
+
+        dirs = ((1,1),(1,-1),(-1,-1),(-1,1))
+        self.generateMovesWithDirs(row,col,dirs,moves)
+
+        
+    def generateMovesWithDirs(self,row,col,dirs,moves):
+        for dir in dirs:
+            for i in range(1,8):
+                endRow = row + dir[0]*i
+                endCol = col + dir[1]*i
+                if 0<=endRow < 8 and 0<= endCol < 8:
+                    if self.board[endRow][endCol] == "--":
+                        moves.append(Move((row,col),(endRow,endCol),self.board))
+                    elif self.board[endRow][endCol][0] != self.board[row][col][0]:
+                        moves.append(Move((row,col),(endRow,endCol),self.board))
+                        break
+                    else:
+                         break
+                else:
+                    break
+
+
+        
+
+    def generateKnightMoves(self,row,col,moves):
+        possibles = [(row-2,col+1),         
+                     (row-2,col-1),
+                     (row-1,col+2),
+                     (row+1,col+2),
+                     (row-1,col-2),
+                     (row+1,col-2),
+                     (row+2,col+1),
+                     (row+2,col-1)]
+        possibles = list(filter(lambda pos: (0<=pos[0]<8 and 0<=pos[1]<8 and self.board[row][col][0] != self.board[pos[0]][pos[1]][0]), possibles))
+        for i in possibles:
+            moves.append(Move((row,col),(i[0],i[1]),self.board))
+        
+
+    def generateQueenMoves(self,row,col,moves):
+        self.generateBishopMoves(row,col,moves)
+        self.generateRookMoves(row,col,moves)
+
+    def generateKingMoves(self,row,col,moves):
+        dirs = ((0,1),(1,0),(1,1),(-1,-1),(0,-1),(-1,0),(1,-1),(-1,1))
+        for i in dirs:
+            endRow = row+i[0]
+            endCol = col+i[1]   
+            if 0<=endRow<8 and 0<=endCol<8:
+                if self.board[row][col][0] != self.board[endRow][endCol][0]:
+                    moves.append(Move((row,col),(endRow,endCol),self.board))
     
        
 
